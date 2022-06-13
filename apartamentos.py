e4847ba44cf3cc84474baa878b0e6b97a9df0b14
pablovns <viniciuspablo962@gmail.com>
2022-06-13 08:39:12 -0300

    Adicionar apartamentos.py e formatar atividade.py

---
+++ apartamentos.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+
+rioAptos = pd.read_csv('https://raw.githubusercontent.com/mvinoba/notebooks-for-binder/master/dados.csv')
+# rioAptos
+
+bairro = st.selectbox('Escolha o bairro', rioAptos['bairro'].unique())
+
+aptos = rioAptos[rioAptos['bairro'] == bairro]
+minimo = int(aptos['preco'].min())
+maximo = int(aptos['preco'].max())
+
+faixa = st.slider("Selecione a faixa de preço", minimo, maximo, value=(minimo, maximo))
+
+st.write(f"Preço minímo: {faixa[0]}")
+st.write(f"Preço máximo: {faixa[1]}")
+
+idx = aptos['preco'].between(faixa[0], faixa[1], inclusive=True)
+st.subheader("Apartamentos dentro dessa faixa de preço")
+aptos[idx]
--- atividade.py
+++ atividade.py
@@ -6,29 +6,28 @@ import geobr
 
 
 st.title("Tópicos Especiais em Informática")
-st.caption("Atividade")
+st.caption("Atividade sobre ")
 
 # download dos geodataframes
 gdf_municipios = geobr.read_municipality()
 gdf_estados = geobr.read_state()
 
-# ordena por nome
 gdf_municipios = gdf_municipios.sort_values(by="name_muni", ascending=True)
 municipios = gdf_municipios["name_muni"]
 
-# escolha dos municípios
+# escolher os municípios
 muni_escolhidos = st.multiselect("Escolha os municípios: ", municipios)
 
 # plotar os municípios escolhidos
 fig, ax = plt.subplots()
 
 gdf_muni_escolhidos = gdf_municipios[gdf_municipios["name_muni"].isin(muni_escolhidos)]
-gdf_muni_escolhidos.plot(ax=ax, color="b", alpha=1)
+gdf_muni_escolhidos.plot(ax=ax, color="k", alpha=1)
 
-gdf_estados_escolhidos = gdf_estados[
-    gdf_estados["abbrev_state"].isin(gdf_muni_escolhidos["abbrev_state"])
-]
-gdf_estados_escolhidos.plot(ax=ax, color="y", alpha=0.5)
+gdf_estados_escolhidos = gdf_estados[gdf_estados["abbrev_state"].isin(gdf_muni_escolhidos["abbrev_state"])]
+gdf_estados_escolhidos.plot(ax=ax, color="g", alpha=0.4)
 
-st.subheader("Mapa dos munícipios escolhidos e seus respectivos estados: ")
+st.subheader("Mapa dos munícipios e seus estados: ")
+
+# apresentar a figura
 st.pyplot(fig)
